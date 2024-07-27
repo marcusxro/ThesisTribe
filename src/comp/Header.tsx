559d917f3982536e206c8405bfb2f5ee7977ac8b
marcusxro <marcussalopaso1@gmail.com>
2024-07-28 00:32:45 +0800

    added filter

--- src/comp/Header.tsx
+++ src/comp/Header.tsx
@@ -20,11 +20,9 @@ const Header: React.FC<typeOfProps> = ({ inputSee }) => {
 
     function searchQuery(e: FormEvent) {
         e.preventDefault()
-
         if (inputVal === '' && !params?.query) {
             return alert("Please search something")
         } else {
-
             nav(`/search/${!inputVal ? params?.query : inputVal}/1`)
             window.scrollTo(0,0)
         }
@@ -32,14 +30,11 @@ const Header: React.FC<typeOfProps> = ({ inputSee }) => {
 
     const [isChanged, setIsChanged] = useState<boolean>(false)
     const inputSearchValue = !inputVal && !isChanged ? params?.query : inputVal
-
-
     return (
         <header className={`custom-pos py-5 ${inputSee && 'border-b-2 border-b-[#e6e6e6]'} `}>
-        
+    
             <div className='flex gap-5 w-full max-w-[800px] items-center'>
-                <div
-                    onClick={() => { nav('/') }}
+                <div onClick={() => { nav('/') }}
                     className={`font-bold text-1xl ${inputSee && 'hidden'} flex items-center gap-1 cursor-pointer md:flex`}><FaSwatchbook />ThesisTribe</div>
                 {
                     inputSee &&
@@ -61,7 +56,7 @@ const Header: React.FC<typeOfProps> = ({ inputSee }) => {
             </div>
             <div
                 onClick={() => { setIsClicked(prevs => !prevs) }}
-                className={`bg-[#292929] text-white py-1 px-7 z-[10000] rounded-3xl cursor-pointer flex gap-1 items-center ${isClicked && 'bg-red-700'} relative`}>
+                className={`hover:bg-gray-700 bg-[#292929] text-white py-1 px-7 z-[10000] rounded-3xl cursor-pointer flex gap-1 items-center ${isClicked && 'bg-red-700'} relative`}>
                 {
                     isClicked ?
                         <><IoMdClose /> Close</> :
@@ -70,7 +65,14 @@ const Header: React.FC<typeOfProps> = ({ inputSee }) => {
             </div>
             {
                 isClicked &&
-                <div className='seeTrough'>
+                <div
+                onClick={() => { setIsClicked(prevs => !prevs) }}
+                 className='seeTrough flex items-end justify-end text-right pr-5 pb-2 leading-[15px] font-bold'>
+                    <span>
+                        Developed &  <br/>
+                        Designed by  <br/>
+                        Marcus
+                    </span>
                 </div>
             }
             {
@@ -79,15 +81,15 @@ const Header: React.FC<typeOfProps> = ({ inputSee }) => {
                     <div
                     onClick={() => {nav('/')}}
                         className='bg-[#292929] text-white py-1 px-5
-                     rounded-3xl cursor-pointer flex gap-1 items-center
+                     rounded-3xl cursor-pointer flex gap-1 items-center hover:bg-gray-700 
                       text-center'><IoIosHome /> Home</div>
                     <div
                         className='bg-[#292929] text-white py-1
-                     px-5 rounded-3xl cursor-pointer flex gap-1 
+                     px-5 rounded-3xl cursor-pointer flex gap-1  hover:bg-gray-700 
                      items-center text-center'><FaInfoCircle /> About</div>
                     <div
                         className='bg-[#292929] text-white
-                     py-1 px-5 rounded-3xl cursor-pointer 
+                     py-1 px-5 rounded-3xl cursor-pointer  hover:bg-gray-700 
                      flex gap-1 items-center text-center'><IoMdContact />Contact</div>
                 </div>
             }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        'auto-fill': 'repeat(auto-fill, minmax(150px, 1fr))',
+      },
+    },
   },
   plugins: [],
 }
