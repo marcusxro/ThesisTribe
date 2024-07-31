4e5cc55213884e8963c709198d077300f0e817ba
marcusxro <marcussalopaso1@gmail.com>
2024-07-31 15:25:40 +0800

    added books col

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import SignInModal from '../comp/SignInModal'
 import Header from '../comp/Header'
+import { onAuthStateChanged } from 'firebase/auth'
+import { authKey } from '../firebase/FirebaseKey'
+import { useNavigate } from 'react-router-dom'
 
 const SignIn: React.FC = () => {
+
+    const nav = useNavigate()
+
+    useEffect(() => {
+        const unsub = onAuthStateChanged(authKey, (creds) => {
+            if(creds) {
+                nav(-1)
+            }
+        })
+
+        return  () => {unsub()}
+    },[])
 
 
     return (
